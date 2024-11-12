ca0fa730bdce50822218ae780b3ea59558d4f3dc
nileshrajbhar731 <nileshrajbhar731@gmail.com>
2024-11-12 09:37:49 +0530

    first commit

---
+++ email-temp/contact_mail_send.php
@@ -0,0 +1,48 @@
+<?php
+require 'vendor/autoload.php'; // Autoload Composer's dependencies
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Load environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+if (isset($_POST['email']) && isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['phone']) && isset($_POST['message'])) {
+    $email = $_POST['email'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->isSMTP();
+        $mail->Host       = $_ENV['MAIL_HOST'];
+        $mail->SMTPAuth   = true;
+        $mail->Username   = $_ENV['MAIL_USERNAME'];
+        $mail->Password   = $_ENV['MAIL_PASSWORD'];
+        $mail->SMTPSecure = $_ENV['MAIL_ENCRYPTION'];
+        $mail->Port       = $_ENV['MAIL_PORT'];
+
+        //Recipients
+        $mail->setFrom($_ENV['MAIL_FROM_ADDRESS'], $_ENV['MAIL_FROM_NAME']);
+        $mail->addAddress($_ENV['MAIL_FROM_ADDRESS']); // Add a recipient email address
+        $mail->addAddress("kcresst28@gmail.com");
+        $mail->addAddress("jasbir64@gmail.com");
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = "Connect with us";
+        $mail->Body    = $message . "<br><br> Full Name: " . $fname." ". $lname .  "<br>Email: " . $email . "<br>Phone: " . $phone ;
+        
+        $mail->send();
+        echo 'Mail has been sent successfully!';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+} else {
+    echo 'Please fill in all details';
+}
